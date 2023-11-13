49a1c6a94ae475b1468a91d81d1100b1891512b3
Bhavesh waghela <mansishah@Mansis-MBP.fritz.box>
2023-11-13 18:23:23 +0100

    Initial commit setup of the project

---
+++ src/main/java/com/live/footballscore/FootballscoreApplication.java
@@ -0,0 +1,13 @@
+package com.live.footballscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballscoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/live/footballscore/FootballscoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.live.footballscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballscoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
